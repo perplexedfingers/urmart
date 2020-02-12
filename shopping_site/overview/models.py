d75b4c01e72206c201e19f68e269d9c50f40c4c2
朱震庭 <r2qokk@users.noreply.github.com>
2020-02-12 15:01:56 +0000

    Add draft

--- shopping_site/overview/models.py
+++ shopping_site/overview/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models):
+    pass
+
+
+class Shop(models):
+    @property
+    def total_sale_gained(self):
+        return sum(product.price * detail.quantity
+                   for product in self.product_set.objects.all()
+                   for detail in product.orderdetail_set.objects.all()
+                   )
+
+    @property
+    def total_orders_taken(self):
+        return sum(detail.order_set.objects.distinct().count()
+                   for product in self.product_set.objects.all()
+                   for detail in product.orderdetail_set.objects.all()
+                   )
+
+    @property
+    def total_quantity_sold(self):
+        return sum(detail.quantity
+                   for product in self.product_set.objects.all()
+                   for detail in product.orderdetail_set.objects.all()
+                   )
+
+
+class Product(models):
+    from_shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    intentory_count = models.IntegerField()
+    price = models.IntegerField()
+    is_vip_only = models.BooleanField()
+
+    @property
+    def ordered(self):
+        return sum(detail.quantity for detail in self.orderdetail_set.objects.all())
+
+    @property
+    def is_available(self):
+        return self.intentory_count > self.ordered
+
+    def is_order_accepted(self, quantity):
+        remaining = self.intentory_count - self.ordered
+        return quantity > remaining
+
+
+class Order(models):
+    by_user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @property
+    def can_be_fullfilled(self):
+        return all((detail.quantity <= detail.product.inventory_count
+                    for detail in self.orderdetail_set.all()))
+
+
+class OrderDetail(models):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
--- shopping_site/overview/tests.py
+++ shopping_site/overview/tests.py
@@ -1,3 +1,93 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class OrderTest(TestCase):
+    def test_add_success(self):
+        vip_product = self.gen_vip_product()
+        normal_product = self.gen_normal_product()
+
+        with self.subTest(msg='VIP order VIP product', product=vip_product, is_vip=True):
+            order = self.order_product(is_vip=is_vip, product=product, quantity=1)
+            self.assertEqual(order.status_code, 204)
+
+        with self.subTest(msg='VIP order normal product', product=normal_product, is_vip=True):
+            order = self.order_product(is_vip=is_vip, product=product, quantity=1)
+            self.assertEqual(order.status_code, 204)
+
+        with self.subTest(msg='Normal user order normal product', product=normal_product, is_vip=False):
+            order = self.order_product(is_vip=is_vip, product=product, quantity=1)
+            self.assertEqual(order.status_code, 204)
+
+    def test_add_fail(self):
+        vip_product = self.gen_vip_product()
+
+        with self.subTest(msg='normal user order VIP product'):
+            order = self.order_product(is_vip=False, product=vip_product, quantity=1)
+            self.assertNotEqual(order.status_code, 204)
+
+        with self.subTest(msg='order too many product'):
+            order = self.order_product(is_vip=True, product=vip_product, quantity=100)
+            self.assertNotEqual(order.status_code, 204)
+
+    def test_remove(self):
+        product = self.gen_normal_product()
+        order = self.order_product(is_vip=False, product=product, quantity=1)
+        product_quantity_after_order = product.quantity
+
+        self.remove_order(order)
+        product.refresh()
+        product_quantity_after_remove = product.quantity
+
+        self.assertEqual(product_quantity_after_order + 1, product_quantity_after_remove)
+
+    def test_order_status_change(self):
+        order = self.gen_order()
+        self.assertTrue(order.can_be_fullfilled)
+        self.item_sold_out(order)
+        self.assertFalse(order.can_be_fullfilled)
+        self.buy_in_item(order)
+        self.assertTrue(order.can_be_fullfilled)
+
+    def gen_vip_product(self):
+        pass
+
+    def gen_normal_product(self):
+        pass
+
+    def order_product(self, is_vip, product, quantity):
+        pass
+
+    def remove_order(self, order):
+        pass
+
+    def item_sold_out(order):
+        pass
+
+    def buy_in_item(order):
+        pass
+
+
+class ReportTest(TestCase):
+    def test_gen_by_shop(self):
+        shop = self.gen_shop()
+        self.make_transaction_for(shop)
+        report = self.gen_report_for(shop)
+        self.assertListEqual(['sale', 'quantity', 'order'], report.keys())
+
+    def test_gen_top_three(self):
+        shop = self.gen_shop()
+        self.make_transaction_for(shop)
+        report = self.get_top_three()
+        self.assertEqual(3, len(report))
+
+    def gen_shop(self):
+        pass
+
+    def make_transaction_for(self, shop):
+        pass
+
+    def gen_report_for(self, shop):
+        pass
+
+    def get_top_three(self):
+        pass
